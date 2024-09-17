86412dd74525a14b96d40fb7372c3afd32da6576
Avin Joy Dcosta <115493294+Avin-7@users.noreply.github.com>
2024-09-17 21:06:17 +0530

    Added db utilsetc

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from "dotenv"
 import  connectDB  from "./db/index.js";
+import app from "./app.js"
+
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB()
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server runnin at port : ${process.env.PORT}`);
+    })
+
+    app.on("error", (error) => {
+        console.log("ERROR : ", error);
+        throw error;
+    })
+}).catch((error) => {
+    console.log(error);
+        throw error;
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+//utility function to handle db connection...
+
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+
+//method 2
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+export {asyncHandler}
